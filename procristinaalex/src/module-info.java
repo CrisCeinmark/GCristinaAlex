ec8fa39d46cd2f77844785212e3efe3cce9fdb43
cris <cris@TARDIS>
2023-04-17 19:11:02 +0200

    Primera version

---
+++ procristinaalex/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cris
+ *
+ */
+module procristinaalex {
+}
---
+++ procristinaalex/src/procristinaalex/HolaMundo.java
@@ -0,0 +1,10 @@
+package procristinaalex;
+
+public class HolaMundo {
+
+	public static void main (String [] args) {
+	
+		System.out.println("Hola Mundo!");
+	
+	}
+}
