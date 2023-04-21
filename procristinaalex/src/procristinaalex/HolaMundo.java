6ef2a6a3026916b45db52886c22227638078ba34
r0manticidio <alexostaruiz@gmail.com>
2023-04-21 23:55:08 +0200

    Quinta version

--- procristinaalex/src/procristinaalex/HolaMundo.java
+++ procristinaalex/src/procristinaalex/HolaMundo.java
@@ -2,15 +2,17 @@ package procristinaalex;
 
 public class HolaMundo {
 
-	public static void main (String [] args) {
-	
+	public static void main(String[] args) {
+
 		System.out.println("Hola Mundo!");
-	
+
 		System.out.println("Hola x2!");
-		
+
 		System.out.println("Hola x3!");
-		
+
 		System.out.println("Hola x4!");
+
+		System.out.println("Hola x5!");
 
 	}
 }
